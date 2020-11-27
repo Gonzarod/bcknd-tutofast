2f9b594619ced80398b9a5426f03700026953b0b
Jesus Duran Huancas <jesusgonzaloduran@gmail.com>
2020-11-27 14:53:49 -0500

    Average Stars for Teacher

--- src/main/java/com/evertix/tutofastbackend/repository/ReviewRepository.java
+++ src/main/java/com/evertix/tutofastbackend/repository/ReviewRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review,Long> {
     Page<Review> getAllByTeacherId(Long teacherId, Pageable pageable);
+
+    List<Review> findAllByTeacherId(Long teacherId);
 }
